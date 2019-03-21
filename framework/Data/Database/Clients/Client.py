85456752ee53e0c3e6a3fe0e40fb491ca1d986ff
Kenneth Sumang <kennethsumang08@gmail.com>
2019-03-21 21:52:41 +0800

    Fixed bug wherein Client will raise a Not Imported exception even though MySQL is not used.

--- framework/Data/Database/Clients/Client.py
+++ framework/Data/Database/Clients/Client.py
@@ -1,10 +1,9 @@
-from framework.Data.Database.Clients.MySQLClient import MySQLClient
-
-
 class Client:
     @staticmethod
     def connect(options):
         if options["client"] == "mysql":
+            from framework.Data.Database.Clients.MySQLClient import MySQLClient
+
             client = MySQLClient(options)
             client.connect()
             return client
