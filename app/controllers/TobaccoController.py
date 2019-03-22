a4c9be4ae698fed45c86162a5501909a9cbb159c
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-03-22 16:23:49 +0800

    A little refactor.

--- app/controllers/TobaccoController.py
+++ app/controllers/TobaccoController.py
@@ -7,56 +7,51 @@ from framework.MVC.Controller import Controller
 class TobaccoController(Controller):
     @staticmethod
     def check_cache(o_app, a_params):
-        filename = o_app.get('APP', 'TOBACCO')['PICKLE_FILE']
+        filename = o_app.get("APP", "TOBACCO")["PICKLE_FILE"]
         exists = blFile.check_if_exists(filename)
         if exists:
-            return Controller.redirect('TobaccoController@get_cache', {})
+            return Controller.redirect("TobaccoController@get_cache", {})
 
-        return Controller.redirect('TobaccoController@fetch_tweets', {})
+        return Controller.redirect("TobaccoController@fetch_tweets", {})
 
     @staticmethod
     def fetch_tweets(o_app, a_params):
-        print('Fetching tobacco tweets...')
-        data = blFetch.fetch_tweets(o_app.get('APP', 'TOBACCO'))
-        if not data['result']:
-            return Controller.error(600, 'Fetching tweets failed!')
-        else:
-            return Controller.redirect('TobaccoController@analyze', {
-                'data': data['data'],
-                'cached': False
-            })
+        print("Fetching tobacco tweets...")
+        data = blFetch.fetch_tweets(o_app.get("APP", "TOBACCO"))
+        if not data["result"]:
+            return Controller.error(600, "Fetching tweets failed!")
+
+        return Controller.redirect(
+            "TobaccoController@analyze", {"data": data["data"], "cached": False}
+        )
 
     @staticmethod
     def get_cache(o_app, a_params):
-        print('Getting tobacco tweets cache...')
-        data = blFile.load_object(o_app.get('APP', 'TOBACCO')['PICKLE_FILE'])
-        if not data['result']:
-            return Controller.error(601, data['message'])
-        else:
-            return Controller.redirect('TobaccoController@analyze', {
-                'data': data['data'],
-                'cached': True
-            })
+        print("Getting tobacco tweets cache...")
+        data = blFile.load_object(o_app.get("APP", "TOBACCO")["PICKLE_FILE"])
+        if not data["result"]:
+            return Controller.error(601, data["message"])
+
+        return Controller.redirect(
+            "TobaccoController@analyze", {"data": data["data"], "cached": True}
+        )
 
     @staticmethod
     def analyze(o_app, a_params):
-        data = a_params['data']
+        data = a_params["data"]
         data = blAnalyze.analyze_tweets(data)
-        return Controller.redirect('TobaccoController@save_data', {
-            'data': data,
-            'cached': a_params['cached']
-        })
+        return Controller.redirect(
+            "TobaccoController@save_data", {"data": data, "cached": a_params["cached"]}
+        )
 
     @staticmethod
     def save_data(o_app, a_params):
-        data = a_params['data']
-        if not a_params['cached']:
-            blFile.save_object('data_tobacco', data)
+        data = a_params["data"]
+        if not a_params["cached"]:
+            blFile.save_object("data_tobacco", data)
 
-        return Controller.redirect('TobaccoController@show_graph', {
-            'data': data
-        })
+        return Controller.redirect("TobaccoController@show_graph", {"data": data})
 
     @staticmethod
     def show_graph(o_app, a_params):
-        print('Showing tobacco graph...')
+        print("Showing tobacco graph...")
--- app/controllers/VapeController.py
+++ app/controllers/VapeController.py
@@ -7,57 +7,52 @@ from framework.MVC.Controller import Controller
 class VapeController(Controller):
     @staticmethod
     def check_cache(o_app, a_params):
-        filename = o_app.get('APP', 'VAPE')['PICKLE_FILE']
+        filename = o_app.get("APP", "VAPE")["PICKLE_FILE"]
         exists = blFile.check_if_exists(filename)
         if exists:
-            return Controller.redirect('VapeController@get_cache', {})
+            return Controller.redirect("VapeController@get_cache", {})
 
-        return Controller.redirect('VapeController@fetch_tweets', {})
+        return Controller.redirect("VapeController@fetch_tweets", {})
 
     @staticmethod
     def fetch_tweets(o_app, a_params):
-        print('Fetching vape tweets...')
-        data = blFetch.fetch_tweets(o_app.get('APP', 'VAPE'))
-        if not data['result']:
-            return Controller.error(600, 'Fetching tweets failed!')
-        else:
-            return Controller.redirect('VapeController@analyze', {
-                'data': data['data'],
-                'cached': False
-            })
+        print("Fetching vape tweets...")
+        data = blFetch.fetch_tweets(o_app.get("APP", "VAPE"))
+        if not data["result"]:
+            return Controller.error(600, "Fetching tweets failed!")
+
+        return Controller.redirect(
+            "VapeController@analyze", {"data": data["data"], "cached": False}
+        )
 
     @staticmethod
     def get_cache(o_app, a_params):
-        print('Getting vape tweets cache...')
-        data = blFile.load_object(o_app.get('APP', 'VAPE')['PICKLE_FILE'])
-        if not data['result']:
-            return Controller.error(601, data['message'])
-        else:
-            return Controller.redirect('VapeController@analyze', {
-                'data': data['data'],
-                'cached': True
-            })
+        print("Getting vape tweets cache...")
+        data = blFile.load_object(o_app.get("APP", "VAPE")["PICKLE_FILE"])
+        if not data["result"]:
+            return Controller.error(601, data["message"])
+
+        return Controller.redirect(
+            "VapeController@analyze", {"data": data["data"], "cached": True}
+        )
 
     @staticmethod
     def analyze(o_app, a_params):
-        data = a_params['data']
+        data = a_params["data"]
         data = blAnalyze.analyze_tweets(data)
-        return Controller.redirect('VapeController@save_data', {
-            'data': data,
-            'cached': a_params['cached']
-        })
+        return Controller.redirect(
+            "VapeController@save_data", {"data": data, "cached": a_params["cached"]}
+        )
 
     @staticmethod
     def save_data(o_app, a_params):
-        data = a_params['data']
-        if not a_params['cached']:
-            blFile.save_object('data_vape', data)
+        data = a_params["data"]
+        if not a_params["cached"]:
+            blFile.save_object("data_vape", data)
 
-        return Controller.redirect('VapeController@show_graph', {
-            'data': data
-        })
+        return Controller.redirect("VapeController@show_graph", {"data": data})
 
     @staticmethod
     def show_graph(o_app, a_params):
-        print('Showing vape graph...')
-        return Controller.redirect('TobaccoController@check_cache', {})
+        print("Showing vape graph...")
+        return Controller.redirect("TobaccoController@check_cache", {})
