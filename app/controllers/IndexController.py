ff3af300f80c25bb122e8b539a3f7e4dd24e4e2e
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-04-01 08:56:46 +0800

    Removed print on IndexController@index.

--- app/controllers/IndexController.py
+++ app/controllers/IndexController.py
@@ -4,7 +4,6 @@ from framework.MVC.Controller import Controller
 class IndexController(Controller):
     @staticmethod
     def index(o_app, a_params):
-        print("This is " + o_app.get("APP", "APP_NAME") + " app.")
         return Controller.redirect("IndexController@redirector", {"mode": "VAPE"})
 
     @staticmethod
